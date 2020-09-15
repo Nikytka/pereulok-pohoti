3631f8a4bf56b14192c0cccfd5bf338d36f1168d
Vlad <vlad.miryaha@gmail.com>
2020-09-16 02:28:10 +0300

    fixit

--- hw03.c
+++ hw03.c
@@ -20,11 +20,19 @@ void Split (char* string, const char* del, char** tokens) {
 int main () {
     char str[] = "jop,d d-a hg- hghf=jnbv";
     const char del[5] = " ,-=";
+  
+    // fixit: нужна константа с понятным именем для числа 10 
     char** token = (char**)malloc(10 * sizeof(char*));
+  
+    // посмотрите, как работает strtok и память под что именно необходимо выделять
     for(int j = 0; j < 10; j++)
         token[j] = (char*) malloc(10 * sizeof(char));
     Split (str, del, token);
+  
+    // fixit: ф-я split должна возвращать число найденных слов. вариант с числом 7 - не является общим решением.
     for(int j = 0; j < 7; j++)
         printf("%s\n", token[j]);
+  
+    // fixit: выделенную память надо освободить
     return 0;
 }

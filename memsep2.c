7c5b147f445c4bfa73c13b4149ed990067a937cb
Nikytka <nechaev.na@phystech.edu>
2020-10-14 12:35:31 +0300

    Lesson 14.10

---
+++ memsep2.c
@@ -0,0 +1,53 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+
+int main()
+{
+   char* array;
+   int shmid;
+   //int new = 1;
+   char pathname[] = "lw08.c";
+   key_t key;
+
+   if((key = ftok(pathname,0)) < 0){
+     printf("Can\'t generate key\n");
+     exit(-1);
+   }
+
+    if ( ( shmid = shmget ( key, 3000 * sizeof ( char ), 0666|IPC_CREAT|IPC_EXCL ) ) < 0 ){
+
+        if ( errno != EEXIST ){
+            printf ( "Can\'t create shared memory\n" );
+            exit ( -1 );
+        }
+        else {
+            if ( ( shmid = shmget ( key, 3000 * sizeof ( char ), 0 ) ) < 0 ){
+                printf ( "Can\'t find shared memory\n" );
+                exit ( -1 );
+            }
+        //new = 0;
+        }
+    }
+
+   if ( ( array = ( char * ) shmat ( shmid, NULL, 0 ) ) == ( char * )( -1 ) ){
+      printf("Can't attach shared memory\n");
+      exit(-1);
+   }
+
+   /*FILE* file = fopen( "06-1a.c" );
+   fread ( array, 3000, file );*/
+
+   printf ( "%s", array );
+
+
+   if ( shmdt ( array ) < 0 ){
+      printf("Can't detach shared memory\n");
+      exit(-1);
+   }
+
+   return 0;
+}
